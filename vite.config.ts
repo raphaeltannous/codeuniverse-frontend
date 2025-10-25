f874bea84f95efaf161bc59efbbf39020e83f1dd
Raphael Tannous <raphaeltannous@proton.me>
2025-10-25 23:19:41 +0300

    Updating vite to silence sass preprocessor warnings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8080,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'import',
+          'color-functions',
+          'global-builtin',
+        ],
+      },
+    },
+  },
 })
