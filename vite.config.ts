4cb24b640362821d79a46056d4e0036ed7653b42
Raphael Tannous <raphaeltannous@proton.me>
2025-11-12 19:50:47 +0200

    Bind to 0.0.0.0 and proxy the backend

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3333/',
+        changeOrigin: true,
+      }
+    }
   },
   css: {
     preprocessorOptions: {
