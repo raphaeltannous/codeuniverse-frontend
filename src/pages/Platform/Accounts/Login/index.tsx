21b7e0286dcf9dbef9d38713f059b61b9cce7749
Raphael Tannous <raphaeltannous@proton.me>
2025-11-19 23:20:06 +0200

    Implementing login post request

--- src/pages/Platform/Accounts/Login/index.tsx
+++ src/pages/Platform/Accounts/Login/index.tsx
@@ -1,14 +1,42 @@
 import { Button, Container, Form, Image } from "react-bootstrap";
 import logo from "../../../../assets/logo.svg";
-import { useState, type ChangeEvent, type FormEvent } from "react";
+import { Activity, useState, type ChangeEvent, type FormEvent } from "react";
 import { NavLink } from "react-router";
+import { useMutation } from "@tanstack/react-query";
 
 interface LoginForm {
   username: string;
   password: string;
 }
 
+type APIError = {
+  code: string;
+  message: string;
+}
+
+type LoginResponse = {
+  username: string; // TODO do we actually need the username?
+  mfaToken: string;
+};
+
 export default function PlatformAccountsLogin() {
+  const loginMutation = useMutation<LoginResponse, APIError, LoginForm>({
+    mutationFn: async (body: LoginForm) => {
+      const res = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+
+      if (!res.ok) {
+        const err = (await res.json()) as APIError;
+        throw err;
+      };
+
+      return (await res.json()) as LoginResponse;
+    },
+  });
+
   const [form, setForm] = useState<LoginForm>({
     username: "",
     password: "",
@@ -16,12 +44,12 @@ export default function PlatformAccountsLogin() {
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setForm({ ...form, [e.target.name]: e.target.value })
-    console.log(form)
   };
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    // TODO: POST Request
+
+    loginMutation.mutate(form);
   };
 
   return (
@@ -48,6 +76,17 @@ export default function PlatformAccountsLogin() {
             placeholder="Password"
             className="mb-3"
           />
+
+          <Activity mode={loginMutation.isSuccess ? 'visible' : 'hidden'}>
+            <div className="text-success mb-3 text-center">
+              User login: {loginMutation.data?.username}
+            </div>
+          </Activity>
+          <Activity mode={loginMutation.isError ? 'visible' : 'hidden'}>
+            <div className="text-danger mb-3 text-center">
+              {loginMutation.error?.message}
+            </div>
+          </Activity>
 
           <Button type="submit" variant="secondary" className="w-100 mb-3">
             Sign In
