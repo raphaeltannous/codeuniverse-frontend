1ebcff787b08ea6c1bc4f5d5844d68d7c30db1f6
Raphael Tannous <raphaeltannous@proton.me>
2025-12-24 16:36:21 +0200

    Implementing polling to check for run and submit state

--- src/pages/Platform/Problems/Problem/editor.tsx
+++ src/pages/Platform/Problems/Problem/editor.tsx
@@ -1,6 +1,6 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { Activity, useState } from "react";
-import { Button, Card, Col, Form, Row, Spinner } from "react-bootstrap";
+import { Alert, Button, Card, Col, Form, Row, Spinner } from "react-bootstrap";
 import { FaCheckCircle, FaTimesCircle } from "react-icons/fa";
 import CodeEditor from "~/components/shared/code-editor";
 import { useAuth } from "~/context/AuthContext";
@@ -24,6 +24,7 @@ export default function ProblemEditor({ problem }: ProblemEditorProps) {
 
   const [submissionId, setSubmissionId] = useState<string | null>(null);
   const [runId, setRunId] = useState<string | null>(null);
+  const [showEditor, setShowEditor] = useState<boolean>(problem.codeSnippets.length != 0);
 
   const runMutation = useMutation<RunResponse, APIError, RunRequest>({
     mutationFn: async (body: RunRequest) => {
@@ -183,110 +184,119 @@ export default function ProblemEditor({ problem }: ProblemEditorProps) {
       <div className="mb-3" dangerouslySetInnerHTML={{ __html: problem.description }}>
       </div>
 
-      <Card className="mb-4">
-        <Card.Header className="d-flex justify-content-between align-items-center">
-          <span>Code</span>
-          <Form.Select
-            value={language}
-            onChange={handleLanguageChange}
-            style={{ width: "150px" }}
-          >
-            {problem.codeSnippets.map((snippet) => (
-              <option key={snippet.languageSlug} value={snippet.languageSlug}>
-                {snippet.languageName}
-              </option>
+      <Activity mode={showEditor ? "visible" : "hidden"}>
+        <Card className="mb-4">
+          <Card.Header className="d-flex justify-content-between align-items-center">
+            <span>Code</span>
+            <Form.Select
+              value={language}
+              onChange={handleLanguageChange}
+              style={{ width: "150px" }}
+            >
+              {problem.codeSnippets.map((snippet) => (
+                <option key={snippet.languageSlug} value={snippet.languageSlug}>
+                  {snippet.languageName}
+                </option>
+              ))}
+            </Form.Select>
+          </Card.Header>
+
+          <Card.Body className="p-0 code-editor-height">
+            <CodeEditor code={code} language={language} onCodeChange={setCode} readonly={false} />
+          </Card.Body>
+
+          <Card.Footer className="d-flex justify-content-end gap-2">
+            <Button variant="primary" onClick={handleRun} disabled={submitMutation.isPending || runMutation.isPending || showSubmitChecking || showRunChecking}>
+              Run
+            </Button>
+            <Button variant="success" onClick={handleSubmit} disabled={submitMutation.isPending || runMutation.isPending || showSubmitChecking || showRunChecking}>
+              Submit
+            </Button>
+          </Card.Footer>
+        </Card>
+
+        <Activity mode={showRunChecking ? "visible" : "hidden"}>
+          <Row className="align-items-center mb-3">
+            <Col>
+              <h5>
+                Running...
+              </h5>
+            </Col>
+            <Col xs="auto">
+              <Spinner animation="border" variant="warning" />
+            </Col>
+          </Row>
+        </Activity>
+
+        <Activity mode={showRunResult ? "visible" : "hidden"}>
+          <Row className="align-items-center mb-3">
+            <Col>
+              <h5>
+                {runStatusQuery.data?.status}
+              </h5>
+            </Col>
+            <Col xs="auto">
+              {runStatusQuery.data?.status === 'ACCEPTED' && (
+                <FaCheckCircle className="text-success" size="1.5em" />
+              )}
+
+              {(runStatusQuery.data?.status === 'FAILED' || runStatusQuery.data?.status === 'ERROR' || runStatusQuery.data?.status == "TIME LIMIT EXCEEDED") && (
+                <FaTimesCircle className="text-danger" size="1.5em" />
+              )}
+            </Col>
+          </Row>
+        </Activity>
+
+        <Activity mode={showSubmitChecking ? "visible" : "hidden"}>
+          <Row className="align-items-center mb-3">
+            <Col>
+              <h5>
+                Submitting...
+              </h5>
+            </Col>
+            <Col xs="auto">
+              <Spinner animation="border" variant="warning" />
+            </Col>
+          </Row>
+        </Activity>
+
+        <Activity mode={showSubmitResult ? "visible" : "hidden"}>
+          <Row className="align-items-center mb-3">
+            <Col>
+              <h5>
+                {submissionStatusQuery.data?.status}
+              </h5>
+            </Col>
+            <Col xs="auto">
+              {submissionStatusQuery.data?.status === 'ACCEPTED' && (
+                <FaCheckCircle className="text-success" size="1.5em" />
+              )}
+
+              {(submissionStatusQuery.data?.status === 'FAILED' || submissionStatusQuery.data?.status === 'ERROR' || submissionStatusQuery.data?.status == "TIME LIMIT EXCEEDED") && (
+                <FaTimesCircle className="text-danger" size="1.5em" />
+              )}
+            </Col>
+          </Row>
+        </Activity>
+
+        <div className="mb-4">
+          <h5>Testcases</h5>
+          <ul className="list-group">
+            {problem.testcases?.map((tc, i) => (
+              <li key={i} className="list-group-item">
+                {tc}
+              </li>
             ))}
-          </Form.Select>
-        </Card.Header>
-
-        <Card.Body className="p-0 code-editor-height">
-          <CodeEditor code={code} language={language} onCodeChange={setCode} readonly={false} />
-        </Card.Body>
-
-        <Card.Footer className="d-flex justify-content-end gap-2">
-          <Button variant="primary" onClick={handleRun} disabled={submitMutation.isPending || runMutation.isPending || showSubmitChecking || showRunChecking}>
-            Run
-          </Button>
-          <Button variant="success" onClick={handleSubmit} disabled={submitMutation.isPending || runMutation.isPending || showSubmitChecking || showRunChecking}>
-            Submit
-          </Button>
-        </Card.Footer>
-      </Card>
-
-      <Activity mode={showRunChecking ? "visible" : "hidden"}>
-        <Row className="align-items-center mb-3">
-          <Col>
-            <h5>
-              Running...
-            </h5>
-          </Col>
-          <Col xs="auto">
-            <Spinner animation="border" variant="warning" />
-          </Col>
-        </Row>
+          </ul>
+        </div>
       </Activity>
-
-      <Activity mode={showRunResult ? "visible" : "hidden"}>
-        <Row className="align-items-center mb-3">
-          <Col>
-            <h5>
-              {runStatusQuery.data?.status}
-            </h5>
-          </Col>
-          <Col xs="auto">
-            {runStatusQuery.data?.status === 'ACCEPTED' && (
-              <FaCheckCircle className="text-success" size="1.5em" />
-            )}
-
-            {(runStatusQuery.data?.status === 'FAILED' || runStatusQuery.data?.status === 'ERROR' || runStatusQuery.data?.status == "TIME LIMIT EXCEEDED") && (
-              <FaTimesCircle className="text-danger" size="1.5em" />
-            )}
-          </Col>
-        </Row>
+      <Activity mode={showEditor ? "hidden" : "visible"}>
+        <Alert className="text-center" key="info" variant="info">
+          <p className="p-0 m-0">
+            This problem doesn't have test cases or execution drivers configured yet.
+          </p>
+        </Alert>
       </Activity>
-
-      <Activity mode={showSubmitChecking ? "visible" : "hidden"}>
-        <Row className="align-items-center mb-3">
-          <Col>
-            <h5>
-              Submitting...
-            </h5>
-          </Col>
-          <Col xs="auto">
-            <Spinner animation="border" variant="warning" />
-          </Col>
-        </Row>
-      </Activity>
-
-      <Activity mode={showSubmitResult ? "visible" : "hidden"}>
-        <Row className="align-items-center mb-3">
-          <Col>
-            <h5>
-              {submissionStatusQuery.data?.status}
-            </h5>
-          </Col>
-          <Col xs="auto">
-            {submissionStatusQuery.data?.status === 'ACCEPTED' && (
-              <FaCheckCircle className="text-success" size="1.5em" />
-            )}
-
-            {(submissionStatusQuery.data?.status === 'FAILED' || submissionStatusQuery.data?.status === 'ERROR' || submissionStatusQuery.data?.status == "TIME LIMIT EXCEEDED") && (
-              <FaTimesCircle className="text-danger" size="1.5em" />
-            )}
-          </Col>
-        </Row>
-      </Activity>
-
-      <div className="mb-4">
-        <h5>Testcases</h5>
-        <ul className="list-group">
-          {problem.testcases?.map((tc, i) => (
-            <li key={i} className="list-group-item">
-              {tc}
-            </li>
-          ))}
-        </ul>
-      </div>
     </div>
   )
 }
